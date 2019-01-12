35a86a822be60b20f99889daae5a5aa9c30abbf9
Pavao Piršljin <pavaopirsljin@gmail.com>
2019-01-12 14:31:23 +0100

    Laravel blog

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+use App\Post;
+use Session;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.posts.index')->with('posts', Post::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        
+        if($categories->count() == 0){
+            
+            Session::flash('info', 'You must have some categories before attempting to make a post.');
+            return redirect()->back();
+        }
+        
+        return view('admin.posts.create')->with('categories', $categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required|max:255',
+            'featured' => 'required|image',
+            'content' => 'required',
+            'category_id' => 'required'
+        ]);
+        
+        $featured = $request->featured;
+        $featured_new_name = time().$featured->getClientOriginalName();
+        $featured->move('uploads/posts', $featured_new_name);
+        
+        $post = Post::create([
+           
+            'title' => $request->title,
+            'content' => $request->content,
+            'featured' => 'uploads/posts/' . $featured_new_name,
+            'category_id' => $request->category_id,
+            'slug' => str_slug($request->title)
+        ]);
+        
+        Session::flash('success', 'Post created succesfully');
+        
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id);        
+        $post->delete();
+        
+        Session::flash('success', 'Post was just trashed');
+        
+        return redirect()->back();
+        
+    }
+    
+    public function trashed(){
+        
+        $posts = Post::onlyTrashed()->get();
+        
+        //Session::flash('success')
+        
+        return view('admin.posts.trashed')->with('posts', $posts);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
+
+    Route::get('/home', [
+        'uses' => 'HomeController@index',
+        'as' => 'home'
+    ]);
+    
+    Route::get('/post/create', [
+    'uses' => 'PostsController@create',
+    'as' => 'post.create'
+    ]);
+
+    Route::post('/post/store', [
+    'uses' => 'PostsController@store',
+    'as' => 'post.store'
+    ]);
+    
+    Route::get('/category/create', [
+        'uses' => 'CategoriesController@create',
+        'as' => 'category.create'
+    ]);
+    
+    Route::get('/posts', [
+        'uses' => 'PostsController@index',
+        'as' => 'posts'
+    ]);
+    
+    Route::get('/posts/trashed', [
+        'uses' => 'PostsController@trashed',
+        'as' => 'posts.trashed'
+    ]);
+    
+    Route::get('/post/delete/{id}', [
+        'uses' => 'PostsController@destroy',
+        'as' => 'post.delete'
+    ]);
+    
+    Route::get('/categories', [
+        'uses' => 'CategoriesController@index',
+        'as' => 'categories'
+    ]);
+    
+    Route::post('/category/store', [
+       'uses' =>  'CategoriesController@store',
+        'as' => 'category.store'
+    ]);
+    
+    Route::get('/category/edit/{id}', [
+        'uses' => 'CategoriesController@edit',
+        'as' => 'category.edit'
+    ]);
+    
+    Route::get('category/delete/{id}', [
+        'uses' => 'CategoriesController@destroy',
+        'as' => 'category.delete'
+    ]);
+    
+    Route::post('/category/update/{id}', [
+       'uses' =>  'CategoriesController@update',
+        'as' => 'category.update'
+    ]);
+    
+});
